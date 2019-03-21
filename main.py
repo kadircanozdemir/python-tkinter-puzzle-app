60432800bc0fcc79ba2a6d60e39a0d32151cf78f
Burak COKYILDIRIM <burak.cokyildirim@ctech.com.tr>
2019-03-21 17:42:43 +0300

    -MainFrame silindi. -class lar main de

--- main.py
+++ main.py
@@ -1,10 +1,91 @@
-import cv2
-import numpy as np
 from tkinter import *
 from tkinter import filedialog
+from PIL import Image, ImageTk
+from tkinter import Entry, Button, OptionMenu
 import random
+import cv2
+import numpy as np
+import os
 
-from MainFrame import MainFrame
+
+class Tiles():
+    def __init__(self):
+        self.tiles = []
+
+    def add(self,tile):
+        self.tiles.append(tile)
+
+    def shuffle(self):
+        random.shuffle(self.tiles)
+
+class Tile(Label):
+    def __init__(self, parent, tileImage, pos):
+        Label.__init__(self, parent, image=image)
+        self.image = image
+        self.pos = pos
+
+class Board(Frame):
+    BOARD_SIZE = 400
+
+    def __init__(self, parent, image, *args, **kwargs):
+        Frame.__init__(self, parent, *args, **kwargs)
+
+        self.parent = parent.geometry("500x500")
+        self.image = self.openImage(image)
+        self.tileSize = self.image.size[0]/4
+        self.tiles = self.createTiles()
+
+    def openImage(self, image):
+        image = Image.open(image)
+        image = image.resize((self.BOARD_SIZE, self.BOARD_SIZE), Image.ANTIALIAS)
+        return image
+
+    def createTiles(self):
+        tiles = Tiles()
+        for row in range(4):
+            for col in range(4):
+                x0 = row*self.tileSize
+                y0 = col*self.tileSize
+                x1 = x0+self.tileSize
+                y1 = y0+self.tileSize
+                tileImage = self.image.crop((x0, y0, x1, y1))
+                tile = Tile(self, tileImage, (row, col))
+                tiles.add(tile)
+        return tiles
+
+
+class Main():
+    def __init__(self, parent):
+        self.parent = parent
+
+        self.image = StringVar()
+
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.mainFrame = Frame(self.parent)
+        Label(self.mainFrame, text='Puzzle Oyunu', font=('', 50)).pack(padx=10, pady=10)
+        frame = Frame(self.mainFrame)
+        Label(frame, text='Image').grid(sticky=W)
+        Entry(frame, textvariable=self.image, width=50).grid(row=0, column=1, pady=10, padx=10)
+        Button(frame, text='Aç', command=self.browse).grid(row=0, column=2, pady=10, padx=10)
+        frame.pack(padx=10, pady=10)
+        Button(self.mainFrame, text='Başla', command=self.start).pack(padx=10, pady=10)
+        self.mainFrame.pack()
+        self.board = Frame(self.parent)
+        self.winFrame = Frame(self.parent)
+
+    def browse(self):
+        self.image.set(filedialog.askopenfilename(title='Please select one (any) frame from your set of images.',
+                                                  filetypes=[('Image Files', ['.jpeg', '.jpg', '.png', '.gif',
+                                                                              '.tiff', '.tif', '.bmp'])]))
+
+    def start(self):
+        image = self.image.get()
+        if os.path.exists(image):
+            self.board = Board(self.parent, image)
+            self.mainFrame.pack_forget()
+            self.board.pack()
 
 
 def chunks(l, n):
@@ -38,33 +119,36 @@ def get_loaction(arr, xy):
                 return i, j
 
 
-def main():
-    # mantıksal hatalar
-    tsetimg = cv2.imread('karisik1.jpg')
-    indexes = np.arange(16)
-    random.shuffle(indexes)
-    j = 0
-    suffled_indexes = []
-    for i in chunks(indexes, 4):
-        suffled_indexes.append(i)
-        j = j + 1
-    print(suffled_indexes)
-    print(get_loaction(suffled_indexes, 11))
-    i, j = get_loaction(suffled_indexes, 11)
-    get_image_piece(tsetimg, i, j)
-    ####
+# def main():
+# mantıksal hatalar
+# tsetimg = cv2.imread('karisik1.jpg')
+# indexes = np.arange(16)
+# random.shuffle(indexes)
+# j = 0
+# suffled_indexes = []
+# for i in chunks(indexes, 4):
+#     suffled_indexes.append(i)
+#     j = j + 1
+# print(suffled_indexes)
+# print(get_loaction(suffled_indexes, 11))
+# i, j = get_loaction(suffled_indexes, 11)
+# get_image_piece(tsetimg, i, j)
+####
 
-    imageA = cv2.imread("/Users/burakcokyildirim/Desktop/Screen Shot 2019-03-15 at 17.53.51.png")
-    imageB = cv2.imread("/Users/burakcokyildirim/Desktop/Screen Shot 2019-03-15 at 17.53.51.png")
-    root = Tk()
-    app = MainFrame(root)
-    app.pack(fill="both", expand=True)
-    root.mainloop()
+# imageA = cv2.imread("/Users/burakcokyildirim/Desktop/Screen Shot 2019-03-15 at 17.53.51.png")
+# imageB = cv2.imread("/Users/burakcokyildirim/Desktop/Screen Shot 2019-03-15 at 17.53.51.png")
+# root = Tk()
+# app = MainFrame(root)
+# app.pack(fill="both", expand=True)
+# root.mainloop()
 
 
 if __name__ == "__main__":
-    main()
-
+    # main()
+    root = Tk()
+    root.title("Puzzle Oyunu")
+    Main(root)
+    root.mainloop()
     '''
     https://www.pyimagesearch.com/2017/06/19/image-difference-with-opencv-and-python/
     https://stackoverflow.com/questions/27035672/cv-extract-differences-between-two-images
