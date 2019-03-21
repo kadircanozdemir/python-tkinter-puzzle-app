7d53ed5004edb16d6a85bce00686e499dd66c99d
kdrozdemir <sylv3r10@gmail.com>
2019-03-21 21:41:07 +0300

    get img boxes

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ from MainFrame import MainFrame
 
 
 def chunks(l, n):
-    for i in range(0, len(l), n):
-        yield l[i:i + n]
+    new_array = []
+    for i in range(n):
+        k = l[0 + i * n:4 + i * n]
+        new_array.append(k)
+    return new_array
 
 
 def test_similar(img1, img2):
@@ -31,35 +34,59 @@ def get_image_piece(img, i, j):
         print("slice error!")
 
 
-def get_loaction(arr, xy):
+def get_ij(arr, ij):
     for i in range(0, 4):
         for j in range(0, 4):
-            if arr[i][j] == xy:
+            if arr[i][j] == ij:
                 return i, j
+
+
+def get_shuffled_image(orj_img, indexes):
+    shuffle_indexes = np.arange(16)
+    random.shuffle(shuffle_indexes)
+    shuffled_indexes = chunks(shuffle_indexes, 4)
+    orginal_indexes = chunks(indexes, 4)
+    # original_image = list()
+    for i in range(0, 4):
+        for j in range(0, 4):
+            point = shuffled_indexes[i][j]
+            r, c = get_ij(orginal_indexes, point)
+            img_box = get_image_piece(orj_img, r, c)
+            # original_image.append(img_box)
+            cv2.imshow("aa", img_box)
+            cv2.waitKey(0)
+    # print(original_image)
+    # cv2.imshow("aa", original_image)
+    # cv2.waitKey(0)
 
 
 def main():
     # mantıksal hatalar
-    tsetimg = cv2.imread('karisik1.jpg')
+    test_image = cv2.imread('karisik1.jpg')
     indexes = np.arange(16)
-    random.shuffle(indexes)
-    j = 0
-    suffled_indexes = []
-    for i in chunks(indexes, 4):
-        suffled_indexes.append(i)
-        j = j + 1
-    print(suffled_indexes)
-    print(get_loaction(suffled_indexes, 11))
-    i, j = get_loaction(suffled_indexes, 11)
-    get_image_piece(tsetimg, i, j)
+
+    print(chunks(indexes, 4))
+    get_shuffled_image(test_image, indexes)
+    # indexes = np.arange(16)
+    # random.shuffle(indexes)
+    # j = 0
+    # suffled_indexes = []
+    # for i in chunks(indexes, 4):
+    #     suffled_indexes.append(i)
+    #     j = j + 1
+    # print(suffled_indexes)
+    # print(get_loaction(suffled_indexes, 11))
+    # i, j = get_loaction(suffled_indexes, 11)
+    # get_image_piece(tsetimg, i, j)
     ####
 
-    imageA = cv2.imread("/Users/burakcokyildirim/Desktop/Screen Shot 2019-03-15 at 17.53.51.png")
-    imageB = cv2.imread("/Users/burakcokyildirim/Desktop/Screen Shot 2019-03-15 at 17.53.51.png")
-    root = Tk()
-    app = MainFrame(root)
-    app.pack(fill="both", expand=True)
-    root.mainloop()
+    # imageA = cv2.imread("karisik1.jpg")
+    # imageB = cv2.imread("karisik2.jpg")
+    # root = Tk()
+    # app = MainFrame(root)
+    # app.pack(fill="both", expand=True)
+    # root.mainloop()
+    # print(test_similar(imageA, imageB))
 
 
 if __name__ == "__main__":
